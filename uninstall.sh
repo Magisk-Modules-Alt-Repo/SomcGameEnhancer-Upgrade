3793c999376d188978b9ec64f064b1142ae028e8
entr0pia <30486766+entr0pia@users.noreply.github.com>
2022-09-16 10:51:32 +0800

    add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+# 这个脚本会在删除模块的时候执行
+
+pm clear com.sonymobile.gameenhancer
+pm clear com.sonymobile.gameenhancer.api
+pm clear com.sonymobile.gameenhancer.monitor
+pm clear com.sonymobile.gameenhancer.browser
